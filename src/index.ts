1efa0590e7a60513f2f5db2f47f90c00d49f130b
Manjado <michalalchimowicz@gmail.com>
2021-11-12 20:32:41 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import BasketStorage from './BasketStorage/BasketStorage';
 import './global-styles.scss';
+import Router from './Router';
 
 declare global {
   interface Window {
@@ -16,13 +17,17 @@ if (frontendButton) {
   frontendButton.addEventListener('click', () => redirectFunction('frontend'));
 }
 
-const backendButton = document.getElementById('fe-button');
+const backendButton = document.getElementById('be-button');
 
 if (backendButton) {
   backendButton.addEventListener('click', () => redirectFunction('backend'));
 }
 
 const storage = new BasketStorage();
+const router = new Router();
+
+router.addRoute({ name: 'frontend', renderFunction: () => console.log('frontend') });
+router.addRoute({ name: 'backend', renderFunction: () => console.log('backend') });
 
 window.basket = storage;
 export {};
